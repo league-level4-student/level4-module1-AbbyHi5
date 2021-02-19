e79290596824dc4b29cd3b8f57474063c10b364c
Abby <Abby@GamingPC>
2021-02-18 17:15:43 -0800

    ENUM

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -4,32 +4,78 @@ package _01_Custom_ArrayList;
 
 public class ArrayList <T>{
 	
+	T[] tArray;
+	int currentLoc = 0;
+	
 	public ArrayList() {
+		
+		tArray = (T[]) new Object[0];
+		
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
-		
-		return null;
+		return tArray[loc];
 	}
 	
 	public void add(T val) {
+		T[] tArray2 = (T[]) new Object[tArray.length+1];
+		tArray2[tArray2.length-1] = val; 
 		
+		for (int i = 0; i < tArray.length; i++) {
+			tArray2[i] = tArray[i];
+		}
+		
+		tArray = tArray2;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
-		
+		T[] tArray2 = (T[]) new Object[tArray.length+1];
+			for (int i = 0; i < tArray2.length; i++) {
+				if (i < loc) {
+					tArray2[i] = tArray[i];
+				}	else if (i > loc) {
+					tArray2[i] = tArray[i-1];
+				} else {
+					tArray2[i] = val;
+				}
+			}
+			tArray = tArray2;
 	}
 	
 	public void set(int loc, T val) throws IndexOutOfBoundsException {
-		
+		tArray[loc] = val;
 	}
 	
 	public void remove(int loc) throws IndexOutOfBoundsException {
+		T[] tArray2 = (T[]) new Object[tArray.length-1];
 		
+		for (int i = 0; i < tArray.length; i++) {
+
+			if(i < loc) {
+				tArray2[i] = tArray[i];
+			}
+
+			else if(i > loc) {
+				tArray2[i-1] = tArray[i];
+			}
+
+			else {
+				continue;
+			}
+		}
+		tArray = tArray2;
 	}
 	
 	public boolean contains(T val) {
-		
+		for (int i = 0; i < tArray.length; i++) {
+			if (tArray[i].equals(val)) {
+				return true;
+			} 
+		}
 		return false;
+	}
+	
+	public int size() {
+		return tArray.length;
 	}
 }
--- src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
+++ src/_03_Switch_Statement_Practice/CustomButtonOptionPanes.java
@@ -12,6 +12,29 @@ public class CustomButtonOptionPanes {
 		String choice = options[input];
 		
 		//use a switch statement to do something cool for each option
-		
+		switch(choice) {
+		case "Sunday":
+			System.out.println("0");
+			break;
+		case "Monday":
+			System.out.println("1");
+			break;
+		case "Tuesday":
+			System.out.println("2");
+			break;
+		case "Wednesday":
+			System.out.println("3");
+			break;
+		case "Thursday":
+			System.out.println("4");
+			break;
+		case "Friday":
+			System.out.println("5");
+			break;
+		case "Saturday":
+			System.out.println("6");
+			break;
+			
+		}
 	}
 }
