03bd9d02a608b8e2671057541e22642e15b0dcf5
Abby <Abby@GamingPC>
2021-02-25 17:19:03 -0800

    work on snake. finish replay and move

--- src/_05_Enum_Stuff/_00_Horoscope.java
+++ src/_05_Enum_Stuff/_00_Horoscope.java
@@ -8,7 +8,63 @@ public class _00_Horoscope {
 	
 	// 2. Write a method that takes in a Zodiac enum object and uses a JOPtionPane to display
 	//    a different horoscope based on the Zodiac's state.
-	
+	public static void horoscope(Zodiac sign) {
+		switch(sign) {
+			case ARIES:{
+				JOptionPane.showMessageDialog(null, "Aries Horoscope");
+				break;
+			}
+			case TAURUS:{
+				JOptionPane.showMessageDialog(null, "Taurs Horoscope");
+				break;
+			}
+			case GEMINI:{
+				JOptionPane.showMessageDialog(null, "Gemini Horoscope");
+				break;
+			}
+			case CANCER:{
+				JOptionPane.showMessageDialog(null, "Cancer Horoscope");
+				break;
+			}
+			case LEO:{
+				JOptionPane.showMessageDialog(null, "Leo Horoscope");
+				break;
+			}
+			case VIRGO:{
+				JOptionPane.showMessageDialog(null, "Virgo Horoscope");
+				break;
+			}
+			case LIBRA:{
+				JOptionPane.showMessageDialog(null, "Libra Horoscope");
+				break;
+			}
+			case SCORPIO:{
+				JOptionPane.showMessageDialog(null, "Scorpio Horoscope");
+				break;
+			}
+			case SAGITTARIUS:{
+				JOptionPane.showMessageDialog(null, "Sagittarius Horoscope");
+				break;
+			}
+			case CAPRICORN:{
+				JOptionPane.showMessageDialog(null, "Capricorn Horoscope");
+				break;
+			}
+			case AQUARIUS:{
+				JOptionPane.showMessageDialog(null, "Aquarius Horoscope");
+				break;
+			}
+			case PISCES:{
+				JOptionPane.showMessageDialog(null, "Pisces Horoscope");
+				break;
+			}
+		}
+	}
 	// 3. Make a main method to test your method
-	
+	public static void main(String[] args) {
+		Zodiac leo = Zodiac.LEO;
+		Zodiac libra = Zodiac.LIBRA;
+		horoscope(leo);
+		horoscope(libra);
+	}
 }
